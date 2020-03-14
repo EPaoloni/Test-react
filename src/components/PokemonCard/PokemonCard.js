f22ffed346aa225a9f775d1af6aea620a53019e7
Ezequiel Paoloni <ezequielpaoloni@outlook.com>
2020-03-14 00:31:32 -0300

    Fixed the pokemon charge at pokemonCard and added a Loading behavior

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,25 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function PokemonCard(props){
 
     const [pokemon, setPokemon] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        updatePokemonData();
+    }, [props.pokemon]);
 
     function updatePokemonData(){
-        if(!pokemon.name){
-            fetch(props.pokemon.url)
-                .then(res => res.json())
-                .then((data) => {
-                    setPokemon(data)
-                    });
-        }
+        setIsLoading(true);
+        fetch(props.pokemon.url)
+            .then(res => res.json())
+            .then((data) => {
+                setPokemon(data)
+                setIsLoading(false)
+                });
     }
 
     if(!pokemon.name){
-        if(props.pokemon){
+        return <tr/>  
+    } else if(isLoading){
+        return(
+            <tr>
+                <td>
+                    <h4></h4>
+                </td>
+                <td>
+                    <h4></h4>
+                </td>
+                <td>
+                    <h4></h4>
+                </td>
+                <td>
+                </td>
+            </tr>  
+        );
+    } else {
+
+        if(props.pokemon.name !== pokemon.name){
             updatePokemonData();
         }
-        return <tr/>
-    } else{
 
         return(
             <tr>
