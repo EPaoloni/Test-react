8d515f858ee422d8717284e77ae776e7228736bc
Ezequiel Paoloni <ezequielpaoloni@outlook.com>
2020-03-13 23:21:59 -0300

    Changed class PokemonList to function

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,47 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PokemonCard from '../PokemonCard/PokemonCard'
 
-class PokemonList extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {pokemonList: [{id:0}]}
-    }
+function PokemonList(props){
 
-    render(){
-        if(this.state.pokemonList[0] && this.props.pokemons[0]){
-            return(
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Type</th>
-                            <th>Image</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.props.pokemons.map(pokemon => 
-                            <PokemonCard key={pokemon.id} pokemon={pokemon}/>
-                        )}
-                    </tbody>
-                </table>
-            );
-        } else {
-            return(
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Name</th>
-                            <th>Type</th>
-                            <th>Image</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </table>
-                )
-        }
+    if(props.pokemons[0]){
+        return(
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Image</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.pokemons.map(pokemon => 
+                        <PokemonCard key={pokemon.id} pokemon={pokemon}/>
+                    )}
+                </tbody>
+            </table>
+        );
+    } else {
+        return(
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Image</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+            )
     }
 }
 
